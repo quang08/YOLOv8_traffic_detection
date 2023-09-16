1be9143fa8adee521e7e28c5b3f1f2683ac43c72
quang <nguyenthequang080203@gmail.com>
2023-09-16 16:49:02 +0700

    add yolo command

--- main.py
+++ main.py
@@ -10,3 +10,16 @@ results = model(source, show=True, conf=0.3, save=True, stream=True)
 
 for r in results:
     print(r.boxes)  # print the Boxes object containing the detection bounding boxes
+
+# yolo task=detect mode=train model=yolov8m.pt data=/content/gdrive/MyDrive/YOLO/data/data.yaml epochs=20 imgsz=640
+# yolo task=detect mode=val model=/content/runs/detect/train/weights/best.pt data=/content/gdrive/MyDrive/YOLO/data
+# yolo task=detect mode=predict model=/content/runs/detect/train/weights/best.pt conf=0.3 source=/content/gdrive
+
+# import glob
+# from IPython.display import Image, display
+
+# for image_path in glob.glob(f'/content/runs/detect/predict/*.jpg'):
+#   display(Image(filename=image_path, height=600))
+#   print('\n')
+
+# yolo task=detect mode=predict model=/content/runs/detect/train/weights/best.pt conf=0.3 source=/content/gdrive
