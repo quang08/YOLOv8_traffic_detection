11c15c284942fd97eb4ada99ec4e9b97024ea994
Nguyen The Quang <84165564+quang08@users.noreply.github.com>
2023-09-16 15:01:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 
 model = YOLO("best_traffic_model.pt")
 
-source = "/Users/quangnguyenthe/Desktop/Academics/ImageProcessing/yolo_object_detection/data/test/videos/test2.mp4"
+source = "data/test/videos/test2.mp4"
 
 results = model(source, show=True, conf=0.3, save=True, stream=True)
 
